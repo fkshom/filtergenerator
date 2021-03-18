006aa170e6b84884cd12558b6c13d9e0e33ed797
Shoma FUKUDA <fkshom@gmail.com>
2021-03-19 00:27:13 +0900

    wip

--- lib/filtergen.rb
+++ lib/filtergen.rb
@@ -27,3 +27,14 @@ end
 require_relative './filtergen/logger'
 require_relative './filtergen/routers'
 require_relative './filtergen/repository'
+
+require 'thor'
+class Filtergen::Cli < Thor
+  desc "parse", "do parse"
+  option :juniper, type: :boolean, aliases: '-j', default: :true, desc: 'files is juniper config'
+  option :vds, type: :boolean, aliases: '-v', default: :false, desc: 'files is vds config'
+  def parse(*files)
+    p options[:type]
+    p files
+  end
+end
